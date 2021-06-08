4efcee4871f34e93139756c9bc3483a761d7881e
HaripriyaRavindran <hpravindran@gmail.com>
2021-06-08 22:22:44 +0530

    commited baby

--- testing/sectionAnimation.js
+++ testing/sectionAnimation.js
@@ -1,55 +1,55 @@
-const scale = {
-    values:[
-        {scale:0, borderRadius:50,opacity:0},       
-    ]
-}
-const right = {
-    values:[
-        {x:900,opacity:0}     
-    ]
-}
-const tween = new TimelineLite();
-tween.add(
-    TweenLite.to(".paraanimate1",1,{
-        bezier: right,
-        ease: Power1.easeInOut
-    })
-);
-tween.add(
-    TweenLite.to(".page1",1,{
-        bezier: scale,
-        ease: Power1.easeInOut
-    })
-);
+// const scale = {
+//     values:[
+//         {scale:0, borderRadius:50,opacity:0},       
+//     ]
+// }
+// const right = {
+//     values:[
+//         {x:900,opacity:0}     
+//     ]
+// }
+// const tween = new TimelineLite();
+// tween.add(
+//     TweenLite.to(".paraanimate1",1,{
+//         bezier: right,
+//         ease: Power1.easeInOut
+//     })
+// );
+// tween.add(
+//     TweenLite.to(".page1",1,{
+//         bezier: scale,
+//         ease: Power1.easeInOut
+//     })
+// );
 
-tween.add(
-    TweenLite.to(".page2",1,{
-        bezier: scale,
-        ease: Power1.easeInOut
-    })
-);
-tween.add(
-    TweenLite.to(".page3",1,{
-        bezier: scale,
-        ease: Power1.easeInOut
-    })
-);
-tween.add(
-    TweenLite.to(".page4",1,{
-        bezier: scale,
-        ease: Power1.easeInOut
-    })
-);
-const controller = new ScrollMagic.Controller();
-const scene = new ScrollMagic.Scene({
-    triggerElement : ".animate",
-    duration: 5000,
-    triggerHook: 0
-})
-.setTween(tween)
-.addIndicators()
-.setPin(".animate")
-.addTo(controller);
+// tween.add(
+//     TweenLite.to(".page2",1,{
+//         bezier: scale,
+//         ease: Power1.easeInOut
+//     })
+// );
+// tween.add(
+//     TweenLite.to(".page3",1,{
+//         bezier: scale,
+//         ease: Power1.easeInOut
+//     })
+// );
+// tween.add(
+//     TweenLite.to(".page4",1,{
+//         bezier: scale,
+//         ease: Power1.easeInOut
+//     })
+// );
+// const controller = new ScrollMagic.Controller();
+// const scene = new ScrollMagic.Scene({
+//     triggerElement : ".animate",
+//     duration: 5000,
+//     triggerHook: 0
+// })
+// .setTween(tween)
+// .addIndicators()
+// .setPin(".animate")
+// .addTo(controller);
 
 
 
@@ -75,4 +75,22 @@ const scene = new ScrollMagic.Scene({
 // .addIndicators()
 // .setPin(".animate")
 // .addTo(controller2);
+
+const t2 = gsap.timeline({ defaults: { ease: "power1.easeinout" } });
+
+t2.to(".page1", { scale:0, borderRadius:50,opacity:0, duration: 1.5, delay: .5});
+t2.to(".page2", { scale:0, borderRadius:50,opacity:0, duration: 1.5});
+t2.to(".page3", { scale:0, borderRadius:50,opacity:0, duration: 1.5 });
+t2.to(".page4", { scale:0, borderRadius:50,opacity:0, duration: 1.5 });
+
+const controller = new ScrollMagic.Controller();
+const scene = new ScrollMagic.Scene({
+    triggerElement : ".animate",
+    duration: 5000,
+    triggerHook: 0.1
+})
+.setTween(t2)
+.addIndicators()
+.setPin(".animate")
+.addTo(controller);
 
